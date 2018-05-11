13b458a576722c23ca84fb820c5806c194307907
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-05-11 14:07:12 +0200

    tests: improve tests on buffer values

--- tests/check_cayenne_lpp.c
+++ tests/check_cayenne_lpp.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <check.h>
 
 #include "../cayenne_lpp.h"
@@ -14,13 +15,6 @@ void setup(void)
 void teardown(void)
 {
     cayenne_lpp_reset(&lpp);
-}
-
-void _convert_buffer(cayenne_lpp_t *lpp, char *buffer)
-{
-    for (uint8_t i = 0; i < lpp->cursor; ++i) {
-        sprintf(&buffer[i * 2], "%02X", lpp->buffer[i]);
-    }
 }
 
 START_TEST(test_cayenne_lpp_init)
@@ -42,9 +36,9 @@ START_TEST(test_cayenne_lpp_digital_input)
 {
     cayenne_lpp_add_digital_input(&lpp, 0, 10);
     ck_assert_int_eq(lpp.cursor, CAYENNE_LPP_DIGITAL_INPUT_SIZE);
-    char buffer[2 * CAYENNE_LPP_DIGITAL_INPUT_SIZE + 1];
-    _convert_buffer(&lpp, buffer);
-    ck_assert_str_eq(buffer, "00000A");
+    uint8_t buffer[CAYENNE_LPP_DIGITAL_INPUT_SIZE] = { 0x00, 0x00, 0x0A };
+    ck_assert_int_eq(memcmp(lpp.buffer,
+                     buffer, CAYENNE_LPP_DIGITAL_INPUT_SIZE), 0);
 }
 END_TEST
 
@@ -52,9 +46,9 @@ START_TEST(test_cayenne_lpp_digital_output)
 {
     cayenne_lpp_add_digital_output(&lpp, 1, 25);
     ck_assert_int_eq(lpp.cursor, CAYENNE_LPP_DIGITAL_OUTPUT_SIZE);
-    char buffer[2 * CAYENNE_LPP_DIGITAL_OUTPUT_SIZE + 1];
-    _convert_buffer(&lpp, buffer);
-    ck_assert_str_eq(buffer, "010119");
+    char buffer[CAYENNE_LPP_DIGITAL_OUTPUT_SIZE] = { 0x01, 0x01, 0x19 };
+    ck_assert_int_eq(memcmp(lpp.buffer,
+                            buffer, CAYENNE_LPP_DIGITAL_OUTPUT_SIZE), 0);
 }
 END_TEST
 
@@ -62,9 +56,9 @@ START_TEST(test_cayenne_lpp_analog_input)
 {
     cayenne_lpp_add_analog_input(&lpp, 0, 5.2);
     ck_assert_int_eq(lpp.cursor, CAYENNE_LPP_ANALOG_INPUT_SIZE);
-    char buffer[2 * CAYENNE_LPP_ANALOG_INPUT_SIZE + 1];
-    _convert_buffer(&lpp, buffer);
-    ck_assert_str_eq(buffer, "00020208");
+    char buffer[CAYENNE_LPP_ANALOG_INPUT_SIZE] = { 0x00, 0x02, 0x02, 0x08 };
+    ck_assert_int_eq(memcmp(lpp.buffer,
+                            buffer, CAYENNE_LPP_ANALOG_INPUT_SIZE), 0);
 }
 END_TEST
 
@@ -72,9 +66,9 @@ START_TEST(test_cayenne_lpp_analog_output)
 {
     cayenne_lpp_add_analog_output(&lpp, 1, 25.1);
     ck_assert_int_eq(lpp.cursor, CAYENNE_LPP_ANALOG_OUTPUT_SIZE);
-    char buffer[2 * CAYENNE_LPP_ANALOG_OUTPUT_SIZE + 1];
-    _convert_buffer(&lpp, buffer);
-    ck_assert_str_eq(buffer, "010309CE");
+    char buffer[CAYENNE_LPP_ANALOG_OUTPUT_SIZE] = { 0x01, 0x03, 0x09, 0xCE };
+    ck_assert_int_eq(memcmp(lpp.buffer,
+                            buffer, CAYENNE_LPP_ANALOG_OUTPUT_SIZE), 0);
 }
 END_TEST
 
@@ -82,9 +76,9 @@ START_TEST(test_cayenne_lpp_luminosity)
 {
     cayenne_lpp_add_luminosity(&lpp, 10, 123);
     ck_assert_int_eq(lpp.cursor, CAYENNE_LPP_LUMINOSITY_SIZE);
-    char buffer[2 * CAYENNE_LPP_LUMINOSITY_SIZE + 1];
-    _convert_buffer(&lpp, buffer);
-    ck_assert_str_eq(buffer, "0A65007B");
+    char buffer[CAYENNE_LPP_LUMINOSITY_SIZE] = { 0x0A, 0x65, 0x00, 0x7B };
+    ck_assert_int_eq(memcmp(lpp.buffer,
+                            buffer, CAYENNE_LPP_LUMINOSITY_SIZE), 0);
 }
 END_TEST
 
@@ -92,9 +86,9 @@ START_TEST(test_cayenne_lpp_presence)
 {
     cayenne_lpp_add_presence(&lpp, 5, 1);
     ck_assert_int_eq(lpp.cursor, CAYENNE_LPP_PRESENCE_SIZE);
-    char buffer[2 * CAYENNE_LPP_PRESENCE_SIZE + 1];
-    _convert_buffer(&lpp, buffer);
-    ck_assert_str_eq(buffer, "056601");
+    char buffer[CAYENNE_LPP_PRESENCE_SIZE] = { 0x05, 0x66, 0x01 };
+    ck_assert_int_eq(memcmp(lpp.buffer,
+                            buffer, CAYENNE_LPP_PRESENCE_SIZE), 0);
 }
 END_TEST
 
@@ -103,9 +97,9 @@ START_TEST(test_cayenne_lpp_temperature)
     ck_assert_int_eq(lpp.cursor, 0);
     cayenne_lpp_add_temperature(&lpp, 1, -4.1);
     ck_assert_int_eq(lpp.cursor, CAYENNE_LPP_TEMPERATURE_SIZE);
-    char buffer[2 * CAYENNE_LPP_TEMPERATURE_SIZE + 1];
-    _convert_buffer(&lpp, buffer);
-    ck_assert_str_eq(buffer, "0167FFD7");
+    char buffer[CAYENNE_LPP_TEMPERATURE_SIZE] = { 0x01, 0x67, 0xFF, 0xD7 };
+    ck_assert_int_eq(memcmp(lpp.buffer,
+                            buffer, CAYENNE_LPP_TEMPERATURE_SIZE), 0);
 }
 END_TEST
 
@@ -113,9 +107,9 @@ START_TEST(test_cayenne_lpp_relative_humidity)
 {
     cayenne_lpp_add_relative_humidity(&lpp, 3, 48.5);
     ck_assert_int_eq(lpp.cursor, CAYENNE_LPP_RELATIVE_HUMIDITY_SIZE);
-    char buffer[2 * CAYENNE_LPP_RELATIVE_HUMIDITY_SIZE + 1];
-    _convert_buffer(&lpp, buffer);
-    ck_assert_str_eq(buffer, "036861");
+    char buffer[CAYENNE_LPP_RELATIVE_HUMIDITY_SIZE] = { 0x03, 0x68, 0x61 };
+    ck_assert_int_eq(memcmp(lpp.buffer,
+                            buffer, CAYENNE_LPP_RELATIVE_HUMIDITY_SIZE), 0);
 }
 END_TEST
 
@@ -123,9 +117,9 @@ START_TEST(test_cayenne_lpp_barometric_pressure)
 {
     cayenne_lpp_add_barometric_pressure(&lpp, 10, 996.23);
     ck_assert_int_eq(lpp.cursor, CAYENNE_LPP_BAROMETRIC_PRESSURE_SIZE);
-    char buffer[2 * CAYENNE_LPP_BAROMETRIC_PRESSURE_SIZE + 1];
-    _convert_buffer(&lpp, buffer);
-    ck_assert_str_eq(buffer, "0A6726EA");
+    char buffer[CAYENNE_LPP_BAROMETRIC_PRESSURE_SIZE] = { 0x0A, 0x67, 0x26, 0xEA };
+    ck_assert_int_eq(memcmp(lpp.buffer,
+                            buffer, CAYENNE_LPP_BAROMETRIC_PRESSURE_SIZE), 0);
 }
 END_TEST
 
@@ -134,9 +128,10 @@ START_TEST(test_cayenne_lpp_accelerometer)
     ck_assert_int_eq(lpp.cursor, 0);
     cayenne_lpp_add_accelerometer(&lpp, 6, 1.234, -1.234, 0);
     ck_assert_int_eq(lpp.cursor, CAYENNE_LPP_ACCELEROMETER_SIZE);
-    char buffer[2 * CAYENNE_LPP_ACCELEROMETER_SIZE + 1];
-    _convert_buffer(&lpp, buffer);
-    ck_assert_str_eq(buffer, "067104D2FB2E0000");
+    char buffer[CAYENNE_LPP_ACCELEROMETER_SIZE] = { 0x06, 0x71, 0x04, 0xD2, \
+                                                    0xFB, 0x2E, 0x00, 0x00 };
+    ck_assert_int_eq(memcmp(lpp.buffer,
+                            buffer, CAYENNE_LPP_ACCELEROMETER_SIZE), 0);
 }
 END_TEST
 
@@ -144,9 +139,10 @@ START_TEST(test_cayenne_lpp_gyrometer)
 {
     cayenne_lpp_add_gyrometer(&lpp, 2, 5.3, 4.2, -2.3);
     ck_assert_int_eq(lpp.cursor, CAYENNE_LPP_GYROMETER_SIZE);
-    char buffer[2 * CAYENNE_LPP_GYROMETER_SIZE + 1];
-    _convert_buffer(&lpp, buffer);
-    ck_assert_str_eq(buffer, "0286021201A3FF1A");
+    char buffer[CAYENNE_LPP_GYROMETER_SIZE] = { 0x02, 0x86, 0x02, 0x12, \
+                                                0x01, 0xA3, 0xFF, 0x1A };
+    ck_assert_int_eq(memcmp(lpp.buffer,
+                            buffer, CAYENNE_LPP_GYROMETER_SIZE), 0);
 }
 END_TEST
 
@@ -155,9 +151,11 @@ START_TEST(test_cayenne_lpp_gps)
     ck_assert_int_eq(lpp.cursor, 0);
     cayenne_lpp_add_gps(&lpp, 1, 42.3519, -87.9094, 10);
     ck_assert_int_eq(lpp.cursor, CAYENNE_LPP_GPS_SIZE);
-    char buffer[2 * CAYENNE_LPP_GPS_SIZE + 1];
-    _convert_buffer(&lpp, buffer);
-    ck_assert_str_eq(buffer, "018806765EF2960A0003E8");
+    char buffer[CAYENNE_LPP_GPS_SIZE] = { 0x01, 0x88, 0x06, 0x76, \
+                                          0x5E, 0xF2, 0x96, 0x0A, \
+                                          0x00, 0x03, 0xE8 };
+    ck_assert_int_eq(memcmp(lpp.buffer,
+                            buffer, CAYENNE_LPP_GPS_SIZE), 0);
 }
 END_TEST
 
